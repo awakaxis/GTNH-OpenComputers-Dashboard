4f93ee902a4e57c2f4843cbc9e1c3151e8b5afd9
awakaxis <awakaxis@gmail.com>
2026-02-04 00:58:38 -0800

    feat: add fastapi webserver

---
+++ server/server.py
@@ -0,0 +1,34 @@
+from random import Random
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+random = Random()
+
+last_response = 0;
+responses = [
+    "Response 1",
+    "Response 2",
+    "Response 3",
+    "Response 4",
+    "Response 5",
+    "Response 6",
+    "Response 7",
+]
+
+# required to avoid cors rejection, https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]
+)
+
+@app.get("/test")
+def test():
+    global last_response
+    global random
+
+    copy = responses.copy()
+    copy.pop(last_response)
+    last_response = random.randint(0, len(responses) - 2)
+    return {"data": copy[last_response]}
+
