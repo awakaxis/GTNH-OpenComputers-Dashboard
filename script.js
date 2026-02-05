cd1e007d9a7a29957dc45cf3c94bc4a85cf5b753
awakaxis <awakaxis@gmail.com>
2026-02-04 17:55:20 -0800

    feat: make new response button read "Fetching..." when waiting for a response from server

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.getElementById("send").addEventListener("click", function (event) {
+  doGet();
+});
+
+function doGet() {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "http://127.0.0.1:8000/test/");
+  xhr.onloadstart = () => {
+    document.getElementById("send").innerHTML = "Fetching...";
+  };
   xhr.onload = () => {
     console.log(xhr.status);
     console.log(xhr.responseText);
     document.getElementById("notice").innerHTML = JSON.parse(xhr.responseText)[
       "data"
     ];
+    document.getElementById("send").innerHTML = "New Response";
   };
   xhr.send();
-});
+}
