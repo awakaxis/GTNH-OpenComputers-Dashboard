401d1329c98422e64e3a9475963d9bdd3ff1b209
awakaxis <awakaxis@gmail.com>
2026-02-14 05:24:40 -0800

    feat: change submit-lsc endpoint from GET to PUT with more params

--- server/server.py
+++ server/server.py
@@ -1,12 +1,13 @@
 from random import Random
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import time
 
 app = FastAPI()
 random = Random()
 
-last_response = 0;
+last_response = 0
 responses = [
     "Response 1",
     "Response 2",
@@ -20,25 +21,36 @@ responses = [
 lsc_eu = "N/A"
 
 # required to avoid cors rejection, https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"]
-)
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
-@app.get("/submit-lsc")
-def submit_lsc(stored_eu: str = "0"):
+
+class EUData(BaseModel):
+    stored: int = 0
+    avg_in: int = 0
+    avg_out: int = 0
+    passive_loss: int = 0
+
+
+@app.put("/submit-lsc")
+def submit_lsc(
+    data: EUData,
+):
     global lsc_eu
-    lsc_eu = stored_eu
+    lsc_eu = data.stored
+
     return {"data": "success"}
+
 
 @app.get("/get-eu")
 def get_eu():
     return {"data": lsc_eu}
 
+
 # example endpoint using query parameters
 @app.get("/echo")
 def echo(text: str = "echo"):
     return {"data": text}
+
 
 @app.get("/test")
 def test():
@@ -51,4 +63,3 @@ def test():
     copy.pop(last_response)
     last_response = random.randint(0, len(responses) - 2)
     return {"data": copy[last_response]}
-
